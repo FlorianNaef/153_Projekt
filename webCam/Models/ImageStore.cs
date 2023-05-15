54dd1a101f481ec43544623a23292b59f29598e9
FlorianNaef <florian.naef@protonmail.com>
2023-05-15 17:22:16 +0200

    initial commit

---
+++ webCam/Models/ImageStore.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace webCamExample_Florian_Naef.Models
+{
+    [Table("ImageStore")]
+    public class ImageStore
+    {
+        [Key]
+        public int ImageID { get; set; }
+
+        public string? ImageBase64String { get; set; }
+     
+        public DateTime? CreateDate { get; set; }
+    }
+}
